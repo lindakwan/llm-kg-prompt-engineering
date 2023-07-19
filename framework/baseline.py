a6713b97e7f722f57cc685b0543fe233f559bc12
Linda Kwan <u6707780@anu.edu.au>
2023-07-20 04:35:04 +1000

    Baseline work in progress - enrichment stage

--- framework/baseline.py
+++ framework/baseline.py
@@ -1,11 +1,17 @@
 import os
 import json
 import csv
+import re
+
+import openai
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 # OpenAI API key
 os.environ["OPENAI_API_KEY"] = "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"
+
+sparql_wd = SPARQLWrapper("https://query.wikidata.org/sparql")
 
 # Load the data
 # file = open("../data/nq_data_simple/train25.json", "r")
@@ -25,7 +31,7 @@ llm = OpenAI(temperature=0)
 # qa_pairs = dict()
 
 # Generate a response for each question
-for i, item in enumerate(data[:5]):
+for i, item in enumerate(data[66:71]):
     question = item['question_text']
     response = llm.predict(question)
 
@@ -34,38 +40,118 @@ for i, item in enumerate(data[:5]):
     # qa_pairs[i]["response"] = response.strip()
 
     # Extract knowledge graph facts from the response
-    llm_facts_prompt = PromptTemplate(
-        input_variables=["response"],
-        template="Extract knowledge graph facts from the following: {response}"
+    llm_facts_json = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You will be provided with text, and your task is to extract RDF triples \
+                and parse them into table format.\nSubject | Predicate | Object"
+            },
+            {
+                "role": "user",
+                "content": f"{response}"
+            }
+        ],
+        temperature=0,
+        max_tokens=256
     )
 
-    llm_facts = llm_facts_prompt.format(response=response)
+    llm_facts = llm_facts_json["choices"][0]["message"]["content"]
 
     # Using the extracted facts, generate some SPARQL queries
-    sparql_prompt = PromptTemplate(
-        input_variables=["facts"],
-        template="Convert the following facts to SPARQL queries: {facts}"
-    )
-
-    sparql_queries = llm(sparql_prompt.format(facts=llm_facts))
-
-    # Generate a SPARQL query for each question
-    # prompt = PromptTemplate(
-    #     input_variables=["question"],
-    #     template="Generate a SPARQL query to extract Wikidata facts for the following question: {question}"
+    # sparql_prompt = PromptTemplate(
+    #     input_variables=["facts"],
+    #     template="Convert each RDF triple to a SPARQL query on wikidata: {facts}"
     # )
 
-    # sparql_query = llm(prompt.format(question=question))
+    # sparql_json = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=[
+    #         {
+    #             "role": "system",
+    #             "content": "You will be provided with a table of RDF triples, and your task is to create a list of \
+    #                        Wikidata SPARQL queries that can be used to verify their existence in the knowledge graph."
+    #                        # \nFor example: 1. SELECT ?subject ?predicate ?object WHERE { ... }"
+    #         },
+    #         {
+    #             "role": "user",
+    #             "content": f"{llm_facts}"
+    #         }
+    #     ],
+    #     temperature=0,
+    #     max_tokens=2000
+    # )
+
+    sparql_json = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You will be provided with a table of RDF triples, and your task is to create a list of \
+                           ASK SPARQL queries on Wikidata that can be used to verify those facts."
+            },
+            {
+                "role": "user",
+                "content": f"{llm_facts}"
+            }
+        ],
+        temperature=0,
+        max_tokens=2000
+    )
+
+    sparql_query_output = sparql_json["choices"][0]["message"]["content"]
+
+    sparql_queries = re.findall(r"\bASK(?:\s+WHERE)?\b\s*\{[^}]*\}", sparql_query_output, re.IGNORECASE)
+
+    # sparql_query = llm(sparql_prompt.format(facts=llm_facts))
+
+    # sparql_wd.setQuery(sparql_query)
+    # sparql_wd.setReturnFormat(JSON)
+    # sparql_results = sparql_wd.query().convert()
 
     print("Q:", question)
     print("A:", response.strip(), "\n")
     print("Facts:", llm_facts, "\n")
-    print("SPARQL:", sparql_queries, "\n")
+    print("SPARQL:", sparql_query_output, "\n")
+    print("Queries:", sparql_queries, "\n")
+
+    sparql_results = []
+
+    for query in sparql_queries:
+        sparql_wd.setQuery(query)
+        sparql_wd.setReturnFormat(JSON)
+        sparql_result = sparql_wd.query().convert()
+        sparql_results.append(sparql_result["boolean"])
+        print("Result:", sparql_result["boolean"], "\n")
+
+    # print("Results:", sparql_results, "\n")
 
     # print("SPARQL:", sparql_query, "\n")
 
     # qa_pairs[i]["llm_facts"] = llm_facts
     # qa_pairs[i]["sparql_queries"] = sparql_queries
+
+    # Identify entities in the question
+    entities_json = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You will be provided with a text, and your task is to extract a list of RDF entities."
+            },
+            {
+                "role": "user",
+                "content": f"{question}"
+            }
+        ],
+        temperature=0,
+        max_tokens=256
+    )
+
+    entities_output = entities_json["choices"][0]["message"]["content"]
+
+    print("Entities:", entities_output)
 
 # Save the QA pairs in a JSON file
 # with open("../output/qa_pairs.json", "w") as f:
