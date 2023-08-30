00a3d07ed131d34c43ddf94eba34aa38ad55f8b3
Linda Kwan <u6707780@anu.edu.au>
2023-08-30 15:25:38 +1000

    Updated entity link and prompt module files

--- framework/utilities/entity_link.py
+++ framework/utilities/entity_link.py
@@ -9,18 +9,34 @@ def fetch_wikidata(params):
         return 'There was and error'
 
 
-def generate_parameters(query):
+def generate_parameters(query, ent_type='item', limit=5):
+    """
+    Generate parameters for the wikidata API
+    :param query: entity or property to match
+    :param ent_type: 'item' (by default) or 'property'
+    :param limit: maximum number of results to return
+    :return: parameters for the wikidata API
+    """
     params = {
         'action': 'wbsearchentities',
         'format': 'json',
         'search': query,
-        'language': 'en'
+        'language': 'en',
+        'type': ent_type,
+        'limit': limit
     }
     return params
 
 
-def fetch_wikidata_from_query(query):
-    params = generate_parameters(query)
+def fetch_wikidata_from_query(query, ent_type='item', limit=5):
+    """
+    Use wikidata API to find matching URIs for an entity or property
+    :param query: entity or property to match
+    :param ent_type: 'item' (by default) or 'property'
+    :param limit: maximum number of results to return
+    :return: the query results
+    """
+    params = generate_parameters(query, ent_type=ent_type, limit=limit)
     data = fetch_wikidata(params)
     data = data.json()
     return data
